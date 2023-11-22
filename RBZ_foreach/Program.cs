81689d73d5042097540f0f07688238f1d903a98e
User <User@K05-1040-P022>
2023-11-22 09:48:45 +0100

    Projektdateien hinzufügen.

---
+++ RBZ_foreach/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RBZ_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
